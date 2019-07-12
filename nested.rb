c510aa6cb95d4dfde79680aeed11cbbbd8d97e17
Cody Shaben <cshaben87@gmail.com>
2019-07-12 20:01:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,8 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto] = {:yukihiro_matsumoto}
+    programmer_hash[:yukihiro_matsumoto] = {}
+    
 end
 
 def changing_alan
