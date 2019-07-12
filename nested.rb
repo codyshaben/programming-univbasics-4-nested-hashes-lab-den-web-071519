5fd87fb4d2f09689861553b837a04a136f44091d
Cody Shaben <cshaben87@gmail.com>
2019-07-12 20:18:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,8 @@ def adding_to_dennis
         }
      }
      
-  programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+  dennis_ritchie[:languages] = "Assembly"
+  dennis_ritchie
   programmer_hash
 
 end
